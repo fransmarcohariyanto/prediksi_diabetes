0d1862f3e571c8cf1061c3e7e45f4ef33bcd47da
fransmarcohariyanto <fransmarcohariyanto@gmail.com>
2025-12-06 21:29:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import joblob
+import joblib
 # Memuat model yang telah disimpan
 model_xgb = joblib.load('xgboost_model.pkl')
 
@@ -32,4 +32,5 @@ if st.button('Prediksi Diabetes'):
         st.success('Anda berisiko diabetes')
     else:
         st.success('Anda tidak berisiko diabetes')
+
 
