3ab6d6d826ca8e4594d6907ab66e6cbbb0bc2dec
fransmarcohariyanto <fransmarcohariyanto@gmail.com>
2025-12-06 22:21:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 
 # Memuat model yang telah disimpan
-model_xgb = joblib.load('xgboost_model.pkl')
+model_xgb = joblib.load('dt_model.pkl')
 
 # Fungsi untuk prediksi
 def predict_diabetes(age, gender, hypertension, heart_disease, smoking_history, bmi, hba1c, glucose):
@@ -40,3 +40,4 @@ if st.button('Prediksi Diabetes'):
         st.success('Anda berisiko diabetes')
     else:
         st.success('Anda tidak berisiko diabetes')
+
