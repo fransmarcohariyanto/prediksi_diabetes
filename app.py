8e61578fa4e4dcfee962dfc4263b15d82a0ec306
fransmarcohariyanto <fransmarcohariyanto@gmail.com>
2025-12-06 21:22:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
 import streamlit as st
-import joblib
 import numpy as np
 import pandas as pd
 
@@ -38,3 +37,4 @@ if st.button('Prediksi Diabetes'):
         st.success('Anda berisiko diabetes')
     else:
         st.success('Anda tidak berisiko diabetes')
+
